816d08498358fcdf6171b5f1668c049c859cf62d
NuoC <nuoc@kth.se>
2022-03-06 19:04:27 +0100

    Start working on advent code again

--- advent_of_code/advent_code.hpp
+++ advent_of_code/advent_code.hpp
@@ -5,40 +5,32 @@
 #include <vector>
 #include <stdint.h>
 #include <chrono>
-
-using VecStrView = std::vector<std::string_view>;
+#include <optional>
 
 struct Timer{
 	public:
 		Timer(){
 			clock = std::chrono::steady_clock::now();
 		}
+		Timer(std::optional<float>& resultPtr)
+			:m_resultPtr(resultPtr)
+		{
+			Timer();
+		}
 		~Timer(){
 			std::chrono::duration<float> duration = std::chrono::steady_clock::now() - clock;
-			std::cout << "Elapsed time: " << duration.count() << std::endl;
+			m_resultPtr = duration.count();
+			std::cout << "Elapsed time: " << m_resultPtr.value_or(-1) << std::endl;
 		}
 	private:
 		std::chrono::time_point<std::chrono::steady_clock> clock;
+		std::optional<float> m_resultPtr;
 };
 
+using VecStrView = std::vector<std::string_view>;
 inline static VecStrView split(std::string_view& inputStr, char delimiter=' '){
 	VecStrView vecStrView;
-	vecStrView.reserve(10);
-	size_t strSize = inputStr.size();
-
-	uint32_t offset = 0, pos=0;
-	while((pos = inputStr.find(delimiter, offset)) && pos < strSize){
-		vecStrView.emplace_back(inputStr.substr(offset, pos - offset));
-		offset = pos + 1;
-	}
-
-	vecStrView.emplace_back(inputStr.substr(offset, strSize - offset));
-	return vecStrView;
-}
-
-inline static std::vector<std::string> split_slow(std::string& inputStr, char delimiter=' '){
-	std::vector<std::string> vecStrView;
-	vecStrView.reserve(10);
+	vecStrView.reserve(100);
 	size_t strSize = inputStr.size();
 
 	uint32_t offset = 0, pos=0;
